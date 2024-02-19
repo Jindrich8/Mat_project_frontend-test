e5325bbf34cd6fcccf0afe2bf4c91f4039ab93b1
Jindřich Svatoň <jindrich.svaton@gmail.com>
2024-02-19 13:33:31 +0100

    Add packages and config vite

--- mat-project-vite-test/src/vite-env.d.ts
+++ mat-project-vite-test/src/vite-env.d.ts
@@ -1,3 +1,15 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+    readonly VITE_SANCTUM_API_URL: string
+    readonly VITE_SANCTUM_CSRF_COOKIE_ROUTE:string
+    readonly VITE_SIGN_IN_ROUTE:string
+    readonly VITE_SIGN_OUT_ROUTE:string
+    readonly VITE_USER_OBJECT_ROUTE:string
+    readonly VITE_TWO_FACTOR_CHANNLANGE_ROUTE:string
+    // more env variables...
+  }
   
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
