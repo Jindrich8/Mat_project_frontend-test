a36da8ad38346f3d37d7be6c335b59606a7d03b2
Jindřich Svatoň <jindrich.svaton@gmail.com>
2024-03-10 12:53:15 +0100

    Add style props to AnswerCmp

--- mat-project-vite-test/src/task/Exercise/Exercises/FillInBlanks/review/components/AnswerCmp.tsx
+++ mat-project-vite-test/src/task/Exercise/Exercises/FillInBlanks/review/components/AnswerCmp.tsx
@@ -1,8 +1,9 @@
 
 import React, { FC } from "react"
 import { AnswerTextCmp } from "../../../../../../components/AnswerText/AnswerTextCmp";
+import { BasicStyledCmpProps } from "../../../../../../types/props/props";
 
-interface Props {
+interface Props extends BasicStyledCmpProps {
 userText:string;
 correctText?:string;
 }
